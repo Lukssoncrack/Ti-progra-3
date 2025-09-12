fcc13d5b1ae18768c4dfd95418f3367eab020aa5
Joaquinlanusse <ljoaquin@udesa.edu.ar>
2025-09-12 14:28:33 -0300

    punto 2 - header y footer

--- proyectoiii/src/Components/Footer/Footer.js
+++ proyectoiii/src/Components/Footer/Footer.js
@@ -1,7 +1,13 @@
 import "./Footer.css"
 import React from 'react'
 
-
+function Footer(){
+    return(
+        <section>
+            <p>Joaquin Lanusse Monasterio, Lucas Silveyra y Maximo Chialvo</p>
+        </section>
+    )
+}
 
 
 
--- proyectoiii/src/Components/Header/Header.js
+++ proyectoiii/src/Components/Header/Header.js
@@ -2,7 +2,20 @@ import React from 'react'
 import "./Header.css"
 import { Link } from 'react-router-dom'
 
-
+function Header(){
+    return(
+        <section>
+            <h2>Nefli</h2>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/favorites'>Favoritos</Link>
+                <Link to='/seccion1'>Seccion 1</Link>
+                <Link to='/seccion2'>Seccion 2</Link>
+                
+            </div>
+        </section>
+    )
+}
 
 
 
