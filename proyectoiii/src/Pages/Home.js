ea5aaf326b8803a92c39126319534f7ca319b1f0
maximochialvo <mchialvo@udesa.edu.ar>
2025-09-13 14:59:59 -0300

    home y .3 en proceso

--- proyectoiii/src/Pages/Home.js
+++ proyectoiii/src/Pages/Home.js
@@ -2,12 +2,77 @@ import React, { Component } from 'react';
 import MovieGrid from '../Components/MovieGrid/MovieGrid';
 import { SearchForm } from '../Components/SearchForm/SearchForm';
 import Loading from '../Components/Loading/Loading';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      popularMovies: [],
+      nowPlayingMovies: [],
+      isLoading: true,
+    };
+  }
 
+  componentDidMount() {
+    let apiPopular =
+  'https://api.themoviedb.org/3/movie/popular?api_key=TU_API_KEY&language=es-ES&page=1';
 
+let apiNowPlaying =
+  'https://api.themoviedb.org/3/movie/now_playing?api_key=TU_API_KEY&language=es-ES&page=1';
+    // Populares
+    fetch(apiPopular)
+      .then(function (response) { return response.json(); })
+      .then((data) => {
+        if (data && data.results) {
+          this.setState({ popularMovies: data.results });
+        }
+      })
+      .catch(function (error) { console.log(error); })
 
+      // En cartelera
+      .then(() => fetch(apiNowPlaying))
+      .then(function (response) { return response.json(); })
+      .then((data) => {
+        if (data && data.results) {
+          this.setState({ nowPlayingMovies: data.results });
+        }
+      })
+      .catch(function (error) { console.log(error); })
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
 
+  render() {
+    let { popularMovies, nowPlayingMovies, isLoading } = this.state;
 
+    if (isLoading) {
+      return <Loading />;
+    }
+
+    return (
+      <>
+        <SearchForm history={this.props.history} />
+
+        <section>
+          <h2>Películas Populares</h2>
+          <div className="link-container">
+            <Link className="ver-mas" to="/populares">Ver más</Link>
+          </div>
+          <MovieGrid movies={popularMovies.slice(0, 5)} />
+        </section>
+
+        <section>
+          <h2>Películas en Cartelera</h2>
+          <div className="link-container">
+            <Link className="ver-mas" to="/cartelera">Ver más</Link>
+          </div>
+          <MovieGrid movies={nowPlayingMovies.slice(0, 5)} />
+        </section>
+      </>
+    );
+  }
+}
 
 export default Home;
