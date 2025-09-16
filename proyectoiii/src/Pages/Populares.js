6e39db50fc22ff7e7d2a9790e9ff6f4adc64ffc1
Joaquinlanusse <ljoaquin@udesa.edu.ar>
2025-09-16 19:39:03 -0300

    la pagina de Populares.js

--- proyectoiii/src/Pages/Populares.js
+++ proyectoiii/src/Pages/Populares.js
@@ -9,44 +9,66 @@ class Populares extends Component {
             peliculas:[],
             busqueda: '',
             cargando: true,
+            pagina: 1,
         }
+        this.API_KEY = "1d647c75a2cc59b2d409129bc1e32ed0"
     }
     componentDidMount(){
-        fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1')
-            .then(response => response.json())
-            .then(data => {
+        this.fetchPeliculas()
+    }
+    
+    fetchPeliculas(){
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${this.API_KEY}&language=en-US&page=${this.state.pagina}`)
+        .then(response => response.json())
+        .then(data => {
                 this.setState({
-                    peliculas: data.results,
+                    peliculas: this.state.peliculas.concat(data.results),
                     cargando: false,
                 });
             })
-            .catch(function(error){
+        .catch(function(error){
                 console.log('Error: ', error);
-                
             })
+    }
+    
+    cargarMas(){
+        this.setState(
+            { pagina: this.state.pagina + 1, cargando: true},
+            () => this.fetchPeliculas()
+        )
     }
 
     render(){
+        if(this.state.cargando){
+            return <Loading/>
+        }
         return(
             <div>
                 <h1>Peliculas populares</h1>
-                {this.state.cargando ? (
-                    <p>Cargando...</p>
-                ) : (
-                    <ul>
-                        {this.state.peliculas.map((pelicula) => (
-                            <li key = {pelicula.id}>
-                                <h3>{pelicula.title}</h3>
-                                <img src=''/> 
-                            </li>
-                        ))}
-                    </ul>
-                )}
+                <form>
+                    <input type = 'text' value={this.state.busqueda} onChange={(evento) => this.setState({busqueda: evento.target.value})}/>
 
+                </form>
+                <ul>
+                    {this.state.peliculas.map((pelicula) => {
+                       if(pelicula.title.toLowerCase().includes(this.state.busqueda.toLocaleLowerCase())){
+                        return(
+                            <li key={pelicula.id}>
+                                <h3>{pelicula.title}</h3>
+                                <img src=''/>
+
+                            </li>
+                        ) 
+                       }else{
+                        return null
+                       }
+
+                    })}
+                </ul>
+                <button  onClick={() => this.cargarMas()}>Cargar mas</button>
             </div>
         )
+    }        
     }
-}
-
 
 export default Populares;
